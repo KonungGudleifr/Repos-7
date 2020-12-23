6d105eaae094ce405b86065883c884bb7300cf72
KonungGudleifr <72839083+KonungGudleifr@users.noreply.github.com>
2020-12-23 22:24:39 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_CHAAAAAAAAA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите накопления студента: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите стипендию студента: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите расходы студента: ");
+            double c = double.Parse(Console.ReadLine());
+            int m = 0;
+            for (double i = a; i > c; i += b)
+            {
+                i -= c;
+                c += c * 0.03;
+                m++;
+            }
+            Console.WriteLine(m + " месяцев сможет прожить студент");
+            Console.ReadKey();
+        }
+    }
+}
